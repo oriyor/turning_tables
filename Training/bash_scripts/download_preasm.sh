edd8cb413c4ab6d05193b31aa19cbed0328de507
oriyor <39461788+oriyor@users.noreply.github.com>
2021-11-02 23:05:47 +0200

    Update download_preasm.sh

--- Training/bash_scripts/download_preasm.sh
+++ Training/bash_scripts/download_preasm.sh
@@ -7,21 +7,21 @@ mkdir -p PReasM-$Sampler-$Size
 cd PReasM-$Sampler-$Size
 
 echo "Downloading config.json..."
-wget https://tabreas.s3.us-west-2.amazonaws.com/PReasM/PReasM-Err-Base/config.json
+wget https://tabreas.s3.us-west-2.amazonaws.com/PReasM/PReasM-$Sampler-$Size/config.json
 
 echo "Downloading optimizer.pt..."
-wget https://tabreas.s3.us-west-2.amazonaws.com/PReasM/PReasM-Err-Base/optimizer.pt
+wget https://tabreas.s3.us-west-2.amazonaws.com/PReasM/PReasM-$Sampler-$Size/optimizer.pt
 
 echo "Downloading pytorch_model.bin..."
-wget https://tabreas.s3.us-west-2.amazonaws.com/PReasM/PReasM-Err-Base/pytorch_model.bin
+wget https://tabreas.s3.us-west-2.amazonaws.com/PReasM/PReasM-$Sampler-$Size/pytorch_model.bin
 
 echo "Downloading scheduler.pt..."
-wget https://tabreas.s3.us-west-2.amazonaws.com/PReasM/PReasM-Err-Base/scheduler.pt
+wget https://tabreas.s3.us-west-2.amazonaws.com/PReasM/PReasM-$Sampler-$Size/scheduler.pt
 
 echo "Downloading trainer_state.json..."
-wget https://tabreas.s3.us-west-2.amazonaws.com/PReasM/PReasM-Err-Base/trainer_state.json
+wget https://tabreas.s3.us-west-2.amazonaws.com/PReasM/PReasM-$Sampler-$Size/trainer_state.json
 
 echo "Downloading training_args.bin..."
-wget https://tabreas.s3.us-west-2.amazonaws.com/PReasM/PReasM-Err-Base/training_args.bin
+wget https://tabreas.s3.us-west-2.amazonaws.com/PReasM/PReasM-$Sampler-$Size/training_args.bin
 
 cd ../..
